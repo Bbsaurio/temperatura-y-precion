78169a4743ad244e1bd472f3c5491497c0e8f5d3
sosag <sosag@GAEL>
2025-02-12 16:47:36 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace temperatura_y_precion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("inserta la temperatura");
+            int Tm = int.Parse(Console.ReadLine());
+            System.Console.WriteLine("inserta la precion");
+            int Pr=int.Parse(Console.ReadLine());
+            if(Pr>200 && Tm>100)
+            {
+                System.Console.WriteLine("alarma");
+            }
+            else
+            {
+                System.Console.WriteLine("normal");
+            }
+            Console.ReadKey();
+        }
+    }
+}
